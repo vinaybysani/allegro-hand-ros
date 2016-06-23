a74eb12915bfbc7bece192e9388f2329adc027e8
Felix Duvallet <felix.duvallet@epfl.ch>
2016-06-23 12:45:35 +0200

    remove rospy dependency

--- allegro_hand/setup.py
+++ allegro_hand/setup.py
@@ -3,8 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['allegro_hand'],
-    package_dir={'': 'src'},
-    requires={'rospy'}
+    package_dir={'': 'src'}
 )
 
 setup(**d)
